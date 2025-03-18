fa6c2e2adf6135ecad8f5ac2633a61327da1b1ca
szymonH <szymonH4679@gmail.com>
2025-03-18 16:05:12 +0100

    Create task class and pushing object to list, try to test it

--- planerDnia/MainWindow.axaml.cs
+++ planerDnia/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace planerDnia;
 
@@ -7,5 +9,32 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
     }
+
+    public List<Task> taskList = new();
+    
+    void CreateTaskButtonClick(object? sender, RoutedEventArgs e)
+    {
+        var name = TaskName.Text;
+        var category = (CategoryComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "Nie wybrano kategorii";
+
+        taskList.Add(new Task(name, category));
+
+
+    }
+
+}
+
+public class Task
+{
+    public static string name;
+    public static string category;
+
+    public Task(string name, string category)
+    {
+        name = name;
+        category = category;
+    }
+    
 }
