91aacc314062ff6458bf7d2bd1316990234f6729
SzymonH <szymonh4679@gmail.com>
2025-03-23 18:56:08 +0100

    Finished function adding tasks to planer

--- planerDnia/MainWindow.axaml.cs
+++ planerDnia/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -21,8 +22,14 @@ public partial class MainWindow : Window
     {
         var name = TaskName.Text;
         var category = (CategoryComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "Nie wybrano kategorii";
-    
-        var task = new Task(name, category);
+        
+        var categoryList = new List<string>();
+        foreach (var item in CategoryComboBox.Items)
+        {
+            categoryList.Add(item.ToString());
+        }
+        
+        var task = new Task(name, category, TaskWrapPanel, categoryList);
         
         TaskWrapPanel.Children.Add(task.stackPanel);
         
@@ -37,26 +44,46 @@ public class Task
     public StackPanel stackPanel;
     public Button removeButton;
     public TextBlock textBlock;
+    public WrapPanel parentPanel;
+    public CheckBox czyUkonczoneCheckBox;
+    public List<string> categoryList;
+    public ComboBox categoryComboBox;
  
 
-    public Task(string inputName, string inputCategory)
+    public Task(string inputName, string inputCategory, WrapPanel parentPanel, List<string> inputCategoryList)
     {
         name = inputName;
         category = inputCategory;
-
+        categoryList = new List<string> { "Nic", "Praca", "Rozrywka", "Trening", "Spotkanie" };
         
         stackPanel = new StackPanel
         {
             Orientation = Orientation.Vertical,
+            Margin = new Thickness(5),
+            Background = Brushes.DarkGray
+        };
+        
+        categoryComboBox = new ComboBox
+        {
+            
+            ItemsSource = categoryList, 
+            SelectedItem = categoryList.Contains(inputCategory) ? inputCategory : "" ,
             Margin = new Thickness(5)
         };
         
         textBlock = new TextBlock
         {
-            Text = $"{inputCategory}: {inputName}",
+            Text = $"{inputName}",
             TextWrapping = TextWrapping.Wrap,
             TextAlignment = TextAlignment.Center,
             HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+        };
+
+        czyUkonczoneCheckBox = new CheckBox
+        {
+            Content = "Czy ukończone: ",
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+            Margin = new Thickness(10)
         };
 
         removeButton = new Button
@@ -64,9 +91,12 @@ public class Task
             Content = "Usuń",
             HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
         };
+        removeButton.Click += (_, _) => parentPanel.Children.Remove(stackPanel);
         
-        stackPanel.Children.Add(removeButton);
+        stackPanel.Children.Add(categoryComboBox);
         stackPanel.Children.Add(textBlock);
+        stackPanel.Children.Add(czyUkonczoneCheckBox);
+        stackPanel.Children.Add(removeButton);
 
     }
     
