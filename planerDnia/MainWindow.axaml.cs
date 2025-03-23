d70fbdfb43b20f57e882577231b5e749c02a4804
SzymonH <szymonh4679@gmail.com>
2025-03-23 17:44:43 +0100

    Added: frontend elements to task class, showing new elements function

--- planerDnia/MainWindow.axaml.cs
+++ planerDnia/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media;
 
 namespace planerDnia;
 
@@ -18,23 +21,53 @@ public partial class MainWindow : Window
     {
         var name = TaskName.Text;
         var category = (CategoryComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "Nie wybrano kategorii";
-
-        taskList.Add(new Task(name, category));
-
-
+    
+        var task = new Task(name, category);
+        
+        TaskWrapPanel.Children.Add(task.stackPanel);
+        
     }
 
 }
 
 public class Task
 {
-    public static string name;
-    public static string category;
+    public string name;
+    public string category;
+    public StackPanel stackPanel;
+    public Button removeButton;
+    public TextBlock textBlock;
+ 
 
-    public Task(string name, string category)
+    public Task(string inputName, string inputCategory)
     {
-        name = name;
-        category = category;
+        name = inputName;
+        category = inputCategory;
+
+        
+        stackPanel = new StackPanel
+        {
+            Orientation = Orientation.Vertical,
+            Margin = new Thickness(5)
+        };
+        
+        textBlock = new TextBlock
+        {
+            Text = $"{inputCategory}: {inputName}",
+            TextWrapping = TextWrapping.Wrap,
+            TextAlignment = TextAlignment.Center,
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+        };
+
+        removeButton = new Button
+        {
+            Content = "Usuń",
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center
+        };
+        
+        stackPanel.Children.Add(removeButton);
+        stackPanel.Children.Add(textBlock);
+
     }
     
 }
